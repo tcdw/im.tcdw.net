9b2c66e82298c80312cc9a126d0670ce452a7ec1
tcdw <tcdw2011@gmail.com>
2026-09-10 18:41:50 +0800

    增加 Open Graph 配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://im.tcdw.net',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
